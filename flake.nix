f100ce7fcd98c6d05c0ba599356b2280e5730d8c
System administrator <you@example.com>
2024-05-08 02:09:02 -0300

    refactoring flake

--- flake.nix
+++ flake.nix
@@ -3,61 +3,62 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    
-   home-manager = {
+
+    home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }: 
-  let
+  outputs = inputs @ {
+    nixpkgs,
+    home-manager,
+    ...
+  }: let
     env = import ./nix/envs/nineveh.nix;
     stringListToEnabledOptions = import resources/nix_functions/stringListToEnabledOptions.nix;
-  in
-  {
+  in {
     nixosConfigurations = {
       #the following variable name must be the current host's variable name, otherwise will raise an error
       ${env.nixosVars.hostName} = nixpkgs.lib.nixosSystem {
-	system = env.nixosVars.system;
-	specialArgs = env;
-	modules = [
-	({ pkgs, ...}: {
-	networking.hostName = env.nixosVars.hostName;
-	time.timeZone = env.nixosVars.timeZone;
-})
-	  ./nix/nixosModules
-	  ({ pkgs, ... }: {
-	    #${env.nixosVars.hostName}.osOpts = stringListToEnabledOptions env.nixosVars.modulesToEnable;
-	    nineveh.system.keyRebindings.enable = true;
-	    nineveh.system.bluetooth.enable = true;
-	  })
-	  # make home-manager as a module of nixos so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
-	  home-manager.nixosModules.home-manager {
-	    home-manager = 
-	    let
-	      mainUser = env.nixosVars.mainUser;
-	    in
-	    {
-	      useGlobalPkgs = true;
-	      useUserPackages = true;
-	      extraSpecialArgs = env;
-	      users.${mainUser} = {
-		imports = [
-		  ./nix/homeModules
-	      ];
-		
-		home = {
-		  username = mainUser;
-		  homeDirectory = "/home/${mainUser}";
-		  stateVersion = "23.11";
-		};
-		programs.home-manager.enable = true;
-		${env.nixosVars.hostName}.home = stringListToEnabledOptions env.homeVars.pkgSets;
-	      }; 
-	    };
-	  }
-	];
+        system = env.nixosVars.system;
+        specialArgs = env;
+        modules = [
+          {
+            networking.hostName = env.nixosVars.hostName;
+            time.timeZone = env.nixosVars.timeZone;
+          }
+          ./nix/nixosModules
+          ({pkgs, ...}: {
+            #${env.nixosVars.hostName}.osOpts = stringListToEnabledOptions env.nixosVars.modulesToEnable;
+            nineveh.system.keyRebindings.enable = true;
+            nineveh.system.bluetooth.enable = true;
+          })
+          # make home-manager as a module of nixos so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = let
+              mainUser = env.nixosVars.mainUser;
+            in {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              extraSpecialArgs = env;
+              users.${mainUser} = {
+                imports = [
+                  ./nix/homeModules
+                ];
+
+                home = {
+                  username = mainUser;
+                  homeDirectory = "/home/${mainUser}";
+                  stateVersion = "23.11";
+                };
+                programs.home-manager.enable = true;
+                ${env.nixosVars.hostName}.home = stringListToEnabledOptions env.homeVars.pkgSets;
+              };
+            };
+          }
+        ];
       };
     };
   };
