e38351b32d212e7145e227392eeb026b04c3cd0a
System administrator <you@example.com>
2024-05-11 16:05:27 -0300

    successful nixos-rebuild; updating home-manager flake input to master in order to match nixpkgs nixos-unstable

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
