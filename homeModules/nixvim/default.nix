1abfa7b2ac29f87dcae611164d9189fac88d72ed
System administrator <you@example.com>
2024-05-11 20:55:42 -0300

    successful nixos-rebuild; setting up nixvim

--- homeModules/nixvim/default.nix
+++ homeModules/nixvim/default.nix
@@ -10,56 +10,59 @@
 in {
   options.${osConfig.networking.hostName}.home.nixvim.enable = lib.mkEnableOption "nixvim";
   config = lib.mkIf cfg.enable {
-  	home = { sessionVariables.EDITOR = "nvim";
-	packages = with pkgs; [
-		alejandra #for formatting nix
-        ];
-	};
+    home = {
+      sessionVariables.EDITOR = "nvim";
+      packages = with pkgs; [
+        alejandra #for formatting nix
+      ];
+    };
 
     programs.nixvim = {
       enable = true;
-	defaultEditor = true;
-	globals.mapleader = " ";
+      defaultEditor = true;
+      globals.mapleader = " ";
 
-    opts = {
-      number = true;         # Show line numbers
-      relativenumber = true; # Show relative line numbers
+      clipboard.providers.wl-copy.enable = true;
 
-      shiftwidth = 2;        # Tab width should be 2
-    highlight = {
-    Comment.fg = "#ff00ff";
-    Comment.bg = "#000000";
-    Comment.underline = true;
-    Comment.bold = true;
-  };
-    };
-	plugins = {
-	cmp = {
-      enable = true;
-      autoEnableSources = true;
-#      sources = [
-#        {name = "nvim_lsp";}
-#        {name = "path";}
-#        {name = "buffer";}
-#        {name = "luasnip";}
-#      ];
-};
+      colorschemes.gruvbox.enable = true;
 
-    lsp = {
-      enable = true;
+      opts = {
+        number = true; # Show line numbers
+        relativenumber = true; # Show relative line numbers
 
-      servers = {
-        tsserver.enable = true;
-
-        lua-ls = {
+        shiftwidth = 2; # Tab width should be 2
+        highlight = {
+          Comment.fg = "#ff00ff";
+          Comment.bg = "#000000";
+          Comment.underline = true;
+          Comment.bold = true;
+        };
+      };
+      plugins = {
+        cmp = {
           enable = true;
-          settings.telemetry.enable = false;
+          autoEnableSources = true;
+          #      sources = [
+          #        {name = "nvim_lsp";}
+          #        {name = "path";}
+          #        {name = "buffer";}
+          #        {name = "luasnip";}
+          #      ];
+        };
+
+        lsp = {
+          enable = true;
+
+          servers = {
+            tsserver.enable = true;
+
+            lua-ls = {
+              enable = true;
+              settings.telemetry.enable = false;
+            };
+          };
         };
       };
     };
-
-};
-};
-
-};
+  };
 }
