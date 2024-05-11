484d86541d004b63634dae40966c499850d8d20c
System administrator <you@example.com>
2024-05-11 20:48:18 -0300

    successful nixos-rebuild; setting up nixvim

--- homeModules/nixvim/default.nix
+++ homeModules/nixvim/default.nix
@@ -10,9 +10,45 @@
 in {
   options.${osConfig.networking.hostName}.home.nixvim.enable = lib.mkEnableOption "nixvim";
   config = lib.mkIf cfg.enable {
+  	home = { sessionVariables.EDITOR = "nvim";
+	packages = with pkgs; [
+		alejandra #for formatting nix
+        ];
+	};
+
     programs.nixvim = {
       enable = true;
 	defaultEditor = true;
+	plugins = {
+	nvim-cmp = {
+      enable = true;
+      autoEnableSources = true;
+#      sources = [
+#        {name = "nvim_lsp";}
+#        {name = "path";}
+#        {name = "buffer";}
+#        {name = "luasnip";}
+#      ];
 };
+
+    lsp = {
+      enable = true;
+
+      servers = {
+        tsserver.enable = true;
+
+        lua-ls = {
+          enable = true;
+          settings.telemetry.enable = false;
+        };
+        rust-analyzer = {
+          enable = true;
+          installCargo = true;
+        };
+      };
+    };
+};
+};
+
 };
 }
