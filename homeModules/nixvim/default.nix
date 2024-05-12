7276df309d8ecb546e2274e7ab240ed5f4cbde22
System administrator <you@example.com>
2024-05-11 21:37:06 -0300

    successful nixos-rebuild; setting up nixvim

--- homeModules/nixvim/default.nix
+++ homeModules/nixvim/default.nix
@@ -30,6 +30,9 @@ in {
         number = true; # Show line numbers
         relativenumber = true; # Show relative line numbers
 
+        viAlias = true;
+        vimAlias = true;
+
         shiftwidth = 2; # Tab width should be 2
         highlight = {
           Comment.fg = "#ff00ff";
@@ -39,28 +42,75 @@ in {
         };
       };
       plugins = {
+        telescope.enable = true;
+        rainbow-delimiters.enable = true;
+        nix.enable = true;
+        sleuth.enable = true;
+        surround.enable = true;
+
         cmp = {
           enable = true;
           autoEnableSources = true;
-          #      sources = [
-          #        {name = "nvim_lsp";}
-          #        {name = "path";}
-          #        {name = "buffer";}
-          #        {name = "luasnip";}
-          #      ];
         };
+
+        cmp-nvim-lsp.enable = true;
+        cmp-nvim-lua.enable = true;
+        cmp-rg.enable = true;
+        cmp-treesitter.enable = true;
+        cmp-zsh.enable = true;
+
+        comment.enable = true;
+
+        gitsigns.enable = true;
+
+        indent-blankline = {
+          enable = true;
+          settings = {
+            exclude = {
+              buftypes = ["terminal" "nofile"];
+              filetypes = ["help"];
+            };
+          };
+        };
+
+        lualine.enable = true;
+
+        neogit.enable = true;
 
         lsp = {
           enable = true;
 
           servers = {
             tsserver.enable = true;
+            dagger.enable = true;
+
+            clangd.enable = true;
+
+            dockerls.enable = true;
+
+            ruff-lsp.enable = true;
+
+            terraformls.enable = true;
+
+            sqls.enable = true;
+
+            yamlls.enable = true;
+
+            gopls.enable = true;
+
+            helm-ls.enable = true;
+            hls.enable = true;
+            java-language-server.enable = true;
 
             lua-ls = {
               enable = true;
               settings.telemetry.enable = false;
             };
           };
+        };
+
+        treesitter = {
+          enable = true;
         };
       };
     };
