5901186888e2d05c252f72edab153a30768bc77b
System administrator <you@example.com>
2024-05-12 21:56:05 -0300

    successful nixos-rebuild; beginning to implement fish config

---
+++ homeModules/fish/default.nix
@@ -0,0 +1,13 @@
+{ config, lib, pkgs, osConfig, ... }:
+
+let cfg = config.${osConfig.networking.hostName}.home.fish;
+in {
+  options.${osConfig.networking.hostName}.home.fish.enable = lib.mkEnableOption "fish";
+  config = lib.mkIf cfg.enable {
+    programs.fish = {
+      shellAliases =  {
+        tree = "eza --all --long --tree";
+      };
+    };
+    };
+}
