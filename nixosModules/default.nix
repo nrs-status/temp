9995dd51386c20251c0c32d2dc3cdca6936f82f5
System administrator <you@example.com>
2024-05-12 23:16:57 -0300

    successful nixos-rebuild; refactoring nixosModules

--- nixosModules/default.nix
+++ nixosModules/default.nix
@@ -1,12 +1,14 @@
 { lib, ... }@inputs:
 let
   utils = import ../utils inputs;
-in
-# @NOTE: Additional modules must be at least staged in git
-{
-  imports = utils.recursivelyListNixFilesExceptThoseInIgnoreList {
+  allNixFilesExceptFirstDefault = utils.recursivelyListNixFilesExceptThoseInIgnoreList {
     dir = ./.;
     ignore = [ ./default.nix ];
   };
+  onlyKeepMainDefaultFileInEachSubdir = lib.filter (x: lib.hasSuffix "default.nix" x) allNixFilesExceptFirstDefault; 
+in
+# @NOTE: Additional modules must be at least staged in git
+{
+  imports = onlyKeepMainDefaultFileInEachSubdir;
 }
 
