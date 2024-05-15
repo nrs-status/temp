f8372855947a628d77b596ad07ee8006b5bed1bd
System administrator <you@example.com>
2024-05-14 22:20:43 -0300

    successful nixos-rebuild; extending nix cheatsheet

--- zeus_olympia/fish/default.nix
+++ zeus_olympia/fish/default.nix
@@ -26,12 +26,12 @@ in
       set fish_cursor_replace underscore
       
       function sudo -d "sudo wrapper that handles aliases"
-        if functions -q -- $argv[1]
-          set -l new_args (string join ' ' -- (string escape -- $argv))
-          set argv fish -c "$new_args"
+        if functions -q -- $argv[1] #checks if the first argument to sudo is a function. the -q option makes the `functions` command run quietly with no outputt
+          set -l new_args (string join ' ' -- (string escape -- $argv)) #set -l defines a local variable which remains local to the function. in this case; the function "sudo". this line creates a new string from the arguments coming after the function passed to sudo
+          set argv fish -c "$new_args" # this line modifies the argv array. in combination with the coming "command" command, it will dictate exactly what will be executed by the function
         end
 
-        command sudo $argv
+        command sudo $argv # executes the command sudo along with the arguments passed
       end
       '';
 
@@ -59,6 +59,7 @@ in
         zn = "cd /etc/nixos";
         znm = "cd /etc/nixos/zeus_olympia";
         ze = "cd /etc/nixos/hanging_gardens_babylon";
+        zc = "cd /etc/nixos/temple_artemis_ephesus/navi/cheatsheets";
 
         tl = "trash-list";
 
