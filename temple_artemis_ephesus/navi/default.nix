61e33c3362ea0838ab31670ef3dd8918c417c6dd
System administrator <you@example.com>
2024-05-15 03:59:17 -0300

    successful nixos-rebuild; implementing navi

--- temple_artemis_ephesus/navi/default.nix
+++ temple_artemis_ephesus/navi/default.nix
@@ -8,17 +8,16 @@ in {
       packages = with pkgs; [
         navi
       ];
-      #file.".local/share/navi/cheats".source = ./cheatsheets;
+      #$USER/.local/share/navi/cheats is the default directory used by navi to store user cheatsheets and the cheatsheets repository downloaded by the `navi repo browse` command
       file.".local/share/navi/cheats".source = pkgs.symlinkJoin {
         name = "navi_cheatsheets_directory";
         paths = let
-          fetchedCheatsRepo = builtins.fetchGit {
+          fetchedCheatsheetsRepo = builtins.fetchGit {
            url = "https://github.com/denisidoro/cheats";
            rev = "1339965e9615ce00174cc308a41279d9c59aa75f";
-         };
+         }; #navi's official cheatsheets repository
         in
-        #lib.filesystem.listFilesRecursive ./cheatsheets ++ [ fetchedCheatsRepo.outPath ];
-        [ fetchedCheatsRepo.outPath ./cheatsheets ];
+        [ fetchedCheatsheetsRepo.outPath ./cheatsheets ];
       };
       };
     };
