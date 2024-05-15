e1fb8087b7edca89082ca46578994f7c57954ea4
System administrator <you@example.com>
2024-05-15 04:41:26 -0300

    successful nixos-rebuild; implementing aliases with sudo

--- zeus_olympia/fish/default.nix
+++ zeus_olympia/fish/default.nix
@@ -8,6 +8,19 @@ in
   config = lib.mkIf cfg.enable { 
     programs.fish = {
       enable = true;
+#
+#      package = pkgs.fish.override {
+#        fishEnvPreInit = "" 
+#      };
+#
+      shellInit = '' function sudo -d "sudo wrapper that handles aliases"
+        if functions -q -- $argv[1] #checks if the first argument to sudo is a function. the -q option makes the `functions` command run quietly with no outputt
+          set -l new_args (string join ' ' -- (string escape -- $argv)) #set -l defines a local variable which remains local to the function. in this case; the function "sudo". this line creates a new string from the arguments coming after the function passed to sudo
+          set argv fish -c "$new_args" # this line modifies the argv array. in combination with the coming "command" command, it will dictate exactly what will be executed by the function
+        end
+
+        command sudo $argv # executes the command sudo along with the arguments passed
+      end'';
 
       interactiveShellInit = ''
       #don't greet
@@ -24,16 +37,7 @@ in
       #set the replace mode cursors to an underscore
       set fish_cursor_replace_one underscore
       set fish_cursor_replace underscore
-      
-      function sudo -d "sudo wrapper that handles aliases"
-        if functions -q -- $argv[1] #checks if the first argument to sudo is a function. the -q option makes the `functions` command run quietly with no outputt
-          set -l new_args (string join ' ' -- (string escape -- $argv)) #set -l defines a local variable which remains local to the function. in this case; the function "sudo". this line creates a new string from the arguments coming after the function passed to sudo
-          set argv fish -c "$new_args" # this line modifies the argv array. in combination with the coming "command" command, it will dictate exactly what will be executed by the function
-        end
-
-        command sudo $argv # executes the command sudo along with the arguments passed
-      end
-      '';
+            '';
 
       shellAliases = {
         cp = "cp -riv";
