74d06e5509ed6d3d2f6989ee6dd494bf985659c1
System administrator <you@example.com>
2024-05-15 01:58:33 -0300

    successful nixos-rebuild; implementing navi

--- temple_artemis_ephesus/navi/default.nix
+++ temple_artemis_ephesus/navi/default.nix
@@ -8,17 +8,19 @@ in {
       packages = with pkgs; [
         navi
       ];
-      file = let
-        homeDir = nixosVars.mainUserHomeDir;
-      in 
-      { 
-        ".local/share/navi/cheats".source = ./cheatsheets; 
-       ".local/share/navi" = { 
-         source = ./empty_dir_holder/test;
-         target = ".local/share/navi/testingfold/test";
-       };
-
+      #file.".local/share/navi/cheats".source = ./cheatsheets;
+      file.".local/share/navi/cheats".source = pkgs.symlinkJoin {
+        name = "navi_cheatsheets_directory";
+        paths = let
+          fetchedCheatsRepo = builtins.fetchGit {
+           url = "https://github.com/denisidoro/cheats";
+           rev = "1339965e9615ce00174cc308a41279d9c59aa75f";
+         };
+        in
+        #lib.filesystem.listFilesRecursive ./cheatsheets ++ [ fetchedCheatsRepo.outPath ];
+        [ fetchedCheatsRepo.outPath ./cheatsheets ];
+      };
       };
     };
-  };
 }
+
