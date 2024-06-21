50d7e160d1f988b61ae9bfa1b3fb24578bda3495
System administrator <you@example.com>
2024-06-21 18:22:05 -0300

    successful nixos-rebuild; adding nodejs

---
+++ temple_artemis_ephesus/javascript/default.nix
@@ -0,0 +1,15 @@
+{ config, lib, pkgs, osConfig, ... }:
+
+let cfg = config.${osConfig.networking.hostName}.home.javascript;
+in 
+  {
+  options.${osConfig.networking.hostName}.home.javascript.enable = lib.mkEnableOption "javascript-related packages";
+  config = lib.mkIf cfg.enable {
+    home = {
+      packages = with pkgs; [
+        nodejs_22
+      ];
+    };
+
+ };
+}
