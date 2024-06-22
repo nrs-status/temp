dcc22e49778bd7487320e0dd10f9d56f1c088db6
System administrator <you@example.com>
2024-06-21 22:22:37 -0300

    successful nixos-rebuild; modding navi and adding python

---
+++ temple_artemis_ephesus/python/default.nix
@@ -0,0 +1,15 @@
+{ config, lib, pkgs, osConfig, ... }:
+
+let cfg = config.${osConfig.networking.hostName}.home.python;
+in 
+  {
+  options.${osConfig.networking.hostName}.home.python.enable = lib.mkEnableOption "python-related packages";
+  config = lib.mkIf cfg.enable {
+    home = {
+      packages = with pkgs; [
+        python312
+      ];
+    };
+
+ };
+}
