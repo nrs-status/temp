ee6c0a1914b11c4b85e62fa89df3773dda8c2520
System administrator <you@example.com>
2024-06-22 23:41:01 -0300

    successful nixos-rebuild; testing if podman works

--- zeus_olympia/docker/default.nix
+++ zeus_olympia/docker/default.nix
@@ -6,6 +6,13 @@ in
 {
   options.${nixosVars.hostName}.system.docker.enable = lib.mkEnableOption "Enable rootless docker";
   config = lib.mkIf cfg.enable { 
+        assertions = [
+      {
+        assertion = !config.${nixosVars.hostName}.system.docker.enable;
+        message = "Attempting to enable docker nixOS module yet podman nixOS module is already enabled";
+      }
+    ];
+
      virtualisation.docker.rootless = {
       enable = true;
       setSocketVariable = true;
--- zeus_olympia/podman/default.nix
+++ zeus_olympia/podman/default.nix
@@ -6,6 +6,14 @@ in
 {
   options.${nixosVars.hostName}.system.podman.enable = lib.mkEnableOption "Enable podman";
   config = lib.mkIf cfg.enable { 
+
+    assertions = [
+      {
+        assertion = !config.${nixosVars.hostName}.system.docker.enable;
+        message = "Attempting to enable podman nixOS module yet docker nixOS module is already enabled";
+      }
+    ];
+
      virtualisation.podman = {
       enable = true;
       dockerCompat = true; #alias 'docker' so podman can act as drop-in replacement
