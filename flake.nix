8a416c107d4863e7a81bfc23d8b3cc75e8727680
System administrator <you@example.com>
2024-06-28 13:07:15 -0300

    successful nixos-rebuild; fixing path definition error

--- flake.nix
+++ flake.nix
@@ -2,17 +2,17 @@
   description = "NixOS configuration";
 
   inputs = {
-   # nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-   nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-	nixvim = {
-        url = "github:nix-community/nixvim";
-        inputs.nixpkgs.follows = "nixpkgs";
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nix-doom-emacs-unstraightened.url = "github:marienz/nix-doom-emacs-unstraightened";
@@ -23,8 +23,6 @@
     };
 
     emacs-overlay.url = "github:nix-community/emacs-overlay/master";
-
-
   };
 
   outputs = inputs @ {
@@ -38,7 +36,7 @@
   }: let
     env = import hanging_gardens_babylon/nineveh;
     hostName = env.nixosVars.hostName;
-    lighthouse_alexandria = import ./lighthouse_alexandria { lib = nixpkgs.lib; };
+    lighthouse_alexandria = import ./lighthouse_alexandria {lib = nixpkgs.lib;};
   in {
     nixosConfigurations = {
       #the following variable name must be the current host's variable name, otherwise will raise an error
@@ -50,76 +48,76 @@
             networking.hostName = env.nixosVars.hostName;
             time.timeZone = env.nixosVars.timeZone;
             nixpkgs.overlays = [
-                emacs-overlay.overlay
-                (final: prev: {
-                })
-              ];
+              emacs-overlay.overlay
+              (final: prev: {
+              })
+            ];
           }
           ./zeus_olympia
           vault-secrets.nixosModules.vault-secrets
           {
-            options.nineveh.system.home-manager.enable = nixpkgs.lib.mkEnableOption "home manager"; 
+            options.nineveh.system.home-manager.enable = nixpkgs.lib.mkEnableOption "home manager";
             config.${hostName}.system = lighthouse_alexandria.stringListToEnabledOptions env.nixosVars.modulesToEnable; #in charge of setting the nixosModule options
           }
-         # make home-manager as a module of nixos so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
-
+          # make home-manager as a module of nixos so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
 
           home-manager.nixosModules.home-manager
-          ({ config, lib, ... }:
-          let 
+          ({
+            config,
+            lib,
+            ...
+          }: let
             inherit env;
-		inherit hostName;
-		mainUser = env.nixosVars.mainUser;
-          in
-        {
-            home-manager = lib.mkIf config.${hostName}.system.home-manager.enable { 
+            inherit hostName;
+            mainUser = env.nixosVars.mainUser;
+          in {
+            home-manager = lib.mkIf config.${hostName}.system.home-manager.enable {
               useGlobalPkgs = true;
               useUserPackages = true;
-extraSpecialArgs = env;
+              extraSpecialArgs = env;
               users.${mainUser} = {
                 imports = [
-			 ./temple_artemis_ephesus
-                         nixvim.homeManagerModules.nixvim
-                         nix-doom-emacs-unstraightened.hmModule
-		 ];
+                  ./temple_artemis_ephesus
+                  nixvim.homeManagerModules.nixvim
+                  nix-doom-emacs-unstraightened.hmModule
+                ];
                 home = {
                   username = mainUser;
                   homeDirectory = "/home/${mainUser}";
                   stateVersion = "23.11";
-                  sessionPath = [ "/home/${mainUser}/alaric_kickdsdown_messi" ];
+                  sessionPath = ["/home/${mainUser}/alaric_kicksdown_messi"];
                 };
                 programs.home-manager.enable = true;
                 ${hostName}.home = lighthouse_alexandria.stringListToEnabledOptions env.homeVars.pkgSets;
               };
             };
           })
-	
 
-   #      home-manager.zeus_olympia.home-manager 
-   #      ({ config, lib, ... }:
-   #      {
-   #      home-manager = 
-   #      let
-   #        mainUser = env.nixosVars.mainUser;
-   #      in nixpkgs.lib.mkIf (config.networking.hostName.homeManager.enable == "true") {
-   #           useGlobalPkgs = true;
-   #           useUserPackages = true;
-   #           extraSpecialArgs = env;
-   #           users.${mainUser} = {
-   #             imports = [
-   #               ./temple_artemis_ephesus
-   #             ];
+          #      home-manager.zeus_olympia.home-manager
+          #      ({ config, lib, ... }:
+          #      {
+          #      home-manager =
+          #      let
+          #        mainUser = env.nixosVars.mainUser;
+          #      in nixpkgs.lib.mkIf (config.networking.hostName.homeManager.enable == "true") {
+          #           useGlobalPkgs = true;
+          #           useUserPackages = true;
+          #           extraSpecialArgs = env;
+          #           users.${mainUser} = {
+          #             imports = [
+          #               ./temple_artemis_ephesus
+          #             ];
 
-   #             home = {
-   #               username = mainUser;
-   #               homeDirectory = "/home/${mainUser}";
-   #               stateVersion = "23.11";
-   #             };
-   #             programs.home-manager.enable = true;
-   #           ${hostName}.home = lighthouse_alexandria.stringListToEnabledOptions env.homeVars.pkgSets; #in charge of setting the homeModule options
-   #           };
-   #         };
-   #       })
+          #             home = {
+          #               username = mainUser;
+          #               homeDirectory = "/home/${mainUser}";
+          #               stateVersion = "23.11";
+          #             };
+          #             programs.home-manager.enable = true;
+          #           ${hostName}.home = lighthouse_alexandria.stringListToEnabledOptions env.homeVars.pkgSets; #in charge of setting the homeModule options
+          #           };
+          #         };
+          #       })
         ];
       };
     };
