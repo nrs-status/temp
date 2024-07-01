ef08e919f72dc38e41610d9e3aae3fc7302dc205
System administrator <you@example.com>
2024-07-01 10:48:02 -0300

    successful nixos-rebuild; adding focus child mod+c keybinding

--- temple_artemis_ephesus/sway/default.nix
+++ temple_artemis_ephesus/sway/default.nix
@@ -233,6 +233,7 @@ in {
           "XF86AudioLowerVolume" = "exec --no-startup-id ${pkgs.pulseaudio}/bin/pactl set-sink-volume 0 -5%";
           "XF86AudioMute" = "exec --no-startup-id ${pkgs.pulseaudio}/bin/pactl set-sink-mute 0 toggle";
           "${modifier}+plus" = "scratchpad show";
+          "${modifier}+c" = "focus child";
         };
 
         startup = [
