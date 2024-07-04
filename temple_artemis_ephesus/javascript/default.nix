8a65899dad34d4e2f4dfd11c1ad83cb94a2c13ed
System administrator <you@example.com>
2024-07-03 22:28:25 -0300

    successful nixos-rebuild; adding formatters for nvim-surround

--- temple_artemis_ephesus/javascript/default.nix
+++ temple_artemis_ephesus/javascript/default.nix
@@ -1,15 +1,19 @@
-{ config, lib, pkgs, osConfig, ... }:
-
-let cfg = config.${osConfig.networking.hostName}.home.javascript;
-in 
-  {
+{
+  config,
+  lib,
+  pkgs,
+  osConfig,
+  ...
+}: let
+  cfg = config.${osConfig.networking.hostName}.home.javascript;
+in {
   options.${osConfig.networking.hostName}.home.javascript.enable = lib.mkEnableOption "javascript-related packages";
   config = lib.mkIf cfg.enable {
     home = {
       packages = with pkgs; [
         nodejs_22
+        prettierd #formatter
       ];
     };
-
- };
+  };
 }
