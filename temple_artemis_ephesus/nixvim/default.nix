1644329e78b7d22813def859be75aea520063c97
System administrator <you@example.com>
2024-07-04 18:02:34 -0300

    successful nixos-rebuild; testing tabout-nvim

--- temple_artemis_ephesus/nixvim/default.nix
+++ temple_artemis_ephesus/nixvim/default.nix
@@ -241,36 +241,40 @@ in {
       extraPlugins = with pkgs.vimPlugins; [nvim-surround tabout-nvim];
 
       extraConfigLua = ''
-            local set= function(name) -- defines a function called 'set' that will automatically configure packages such that set "package" is equivalent to require('package').setup()
-              local ok, p = pcall(require, name) -- assigns the return value of pcall(require, name) to ok, p
-              if ok then
-                p.setup()
-              end
-            end
+                local set= function(name) -- defines a function called 'set' that will automatically configure packages such that set "package" is equivalent to require('package').setup()
+                  local ok, p = pcall(require, name) -- assigns the return value of pcall(require, name) to ok, p
+                  if ok then
+                    p.setup()
+                  end
+                end
 
-            require'nvim-surround'.setup({
-              aliases = {
-                ["c"] = "}",
-              },
-            })
+                require'nvim-surround'.setup({
+                  aliases = {
+                    ["c"] = "}",
+                  },
+                })
 
-            require'tabout'.setup {
+                require('tabout').setup {
             tabkey = '<Tab>', -- key to trigger tabout, set to an empty string to disable
-        backwards_tabkey = '<S-Tab>', -- key to trigger backwards tabout, set to an empty string to disable
-        act_as_tab = true, -- shift content if tab out is not possible
-        act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
-        default_tab = '<C-t>', -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
-        default_shift_tab = '<C-d>', -- reverse shift default action,
-        enable_backwards = true, -- well ...
-        completion = true, -- if the tabkey is used in a completion pum
-        tabouts = {
-          {open = "'", close = "'"},
-          {open = '"', close = '"'},
-          {open = '`', close = '`'},
-          {open = '(', close = ')'},
-          {open = '[', close = ']'},
-          {open = '{', close = '}'}
+            backwards_tabkey = '<S-Tab>', -- key to trigger backwards tabout, set to an empty string to disable
+            act_as_tab = true, -- shift content if tab out is not possible
+            act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
+            default_tab = '<C-t>', -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
+            default_shift_tab = '<C-d>', -- reverse shift default action,
+            enable_backwards = true, -- well ...
+            completion = true, -- if the tabkey is used in a completion pum
+            tabouts = {
+              {open = "'", close = "'"},
+              {open = '"', close = '"'},
+              {open = '`', close = '`'},
+              {open = '(', close = ')'},
+              {open = '[', close = ']'},
+              {open = '{', close = '}'}
+            },
+            ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
+            exclude = {} -- tabout will ignore these filetypes
         }
+
       '';
       plugins = {
         hop = {
