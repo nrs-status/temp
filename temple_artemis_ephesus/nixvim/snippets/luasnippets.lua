37a5d5c489980efb5292fad1f8762d86c8710d9a
System administrator <you@example.com>
2024-07-05 01:11:06 -0300

    successful nixos-rebuild; adding snippets in lua

--- temple_artemis_ephesus/nixvim/snippets/luasnippets.lua
+++ temple_artemis_ephesus/nixvim/snippets/luasnippets.lua
@@ -2,11 +2,11 @@ local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
 
-ls.add_snippet("all", {
+ls.add_snippets("all", {
 	s("d", { t("$") })
 })
 
 
-ls.add_snippet("all", {
+ls.add_snippets("all", {
 	s("ddd", { t("this b what i b inserting") })
 })
