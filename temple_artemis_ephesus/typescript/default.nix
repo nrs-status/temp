e74f4a958e9afb2f9befb7e5a4b591939e3c8192
System administrator <you@example.com>
2024-07-11 15:15:51 -0300

    successful nixos-rebuild; temporarily disabling ts-node to see if it will work in local project

--- temple_artemis_ephesus/typescript/default.nix
+++ temple_artemis_ephesus/typescript/default.nix
@@ -1,16 +1,19 @@
-{ config, lib, pkgs, osConfig, ... }:
-
-let cfg = config.${osConfig.networking.hostName}.home.typescript;
-in 
-  {
+{
+  config,
+  lib,
+  pkgs,
+  osConfig,
+  ...
+}: let
+  cfg = config.${osConfig.networking.hostName}.home.typescript;
+in {
   options.${osConfig.networking.hostName}.home.typescript.enable = lib.mkEnableOption "typescript-related packages";
   config = lib.mkIf cfg.enable {
     home = {
       packages = with pkgs; [
         typescript
-        nodePackages_latest.ts-node
+        #        nodePackages_latest.ts-node
       ];
     };
-
- };
+  };
 }
