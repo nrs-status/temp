945586f3d7e97ae7ac0696640394f18bc6926f57
System administrator <you@example.com>
2024-07-22 15:01:55 -0300

    successful nixos-rebuild; opening port 8081

--- zeus_olympia/firewall/default.nix
+++ zeus_olympia/firewall/default.nix
@@ -17,28 +17,5 @@ in {
         allowedTCPPorts = [8081];
       };
     };
-    services.blueman.enable = true;
-
-    environment.systemPackages = with pkgs; [
-      bluez
-      blueman
-    ];
-
-    home-manager = lib.mkIf config.${nixosVars.hostName}.system.home-manager.enable {
-      users.${nixosVars.mainUser}.${nixosVars.hostName}.home.waybar.bluetoothModule.enable = true;
-    };
-
-    # passwordless access to rfkill without sudo so bluetooth can be toggled
-    security.sudo.extraRules = [
-      {
-        groups = ["wheel"];
-        commands = [
-          {
-            command = "/run/current-system/sw/bin/rfkill";
-            options = ["NOPASSWD"];
-          }
-        ];
-      }
-    ];
   };
 }
