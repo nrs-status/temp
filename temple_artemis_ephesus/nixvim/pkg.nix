1f74dbab1a7465b1afb49f095a58dea212ec7e2d
System administrator <you@example.com>
2024-07-25 05:18:15 -0300

    successful nixos-rebuild; tab config to work with cmp in nixvim

--- temple_artemis_ephesus/nixvim/pkg.nix
+++ temple_artemis_ephesus/nixvim/pkg.nix
@@ -56,72 +56,73 @@ nixvim.legacyPackages.${system}.makeNixvimWithModule {
     extraPlugins = with pkgs.vimPlugins; [nvim-surround];
 
     extraConfigLua = ''
-              local set= function(name) -- defines a function called 'set' that will automatically configure packages such that set "package" is equivalent to require('package').setup()
-                local ok, p = pcall(require, name) -- assigns the return value of pcall(require, name) to ok, p
-                if ok then
-                  p.setup()
-                end
-              end
+                 local set= function(name) -- defines a function called 'set' that will automatically configure packages such that set "package" is equivalent to require('package').setup()
+                   local ok, p = pcall(require, name) -- assigns the return value of pcall(require, name) to ok, p
+                   if ok then
+                     p.setup()
+                   end
+                 end
 
-              require'nvim-surround'.setup({
-                aliases = {
-                  ["c"] = "}",
-                  ["p"] = ")",
-                },
-              })
+                 require'nvim-surround'.setup({
+                   aliases = {
+                     ["c"] = "}",
+                     ["p"] = ")",
+                   },
+                 })
 
-      function check_and_insert_space() -- function for <Tab> key
-          -- Get the current cursor position
-          local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+         function check_and_insert_space() -- function for <Tab> key
+             -- Get the current cursor position
+             local row, col = unpack(vim.api.nvim_win_get_cursor(0))
 
-          -- Get the current line
-          local line = vim.api.nvim_get_current_line()
+             -- Get the current line
+             local line = vim.api.nvim_get_current_line()
 
-          -- Check if completion menu is currently open. If it is, call the completion behaviour
-          --            ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+             -- Check if completion menu is currently open. If it is, call the completion behaviour
+             --            ['<Tab>'] = cmp.mapping.confirm({ select = true }),
 
-          print("hello world")
-          local cmp = require'cmp'
-          print(cmp.visible())
-          if cmp.visible() then
-              cmp.close()
-              cmp.confirm({select = true})
-          end
+             print("hello world")
+             local cmp = require'cmp'
+             cmp.complete()
+      --       print(cmp.visible())
+      --       if cmp.visible() then
+      --           cmp.close()
+      --           -- cmp.confirm({select = true})
+      --       end
 
-          -- Check if there's whitespace before the cursor
-          local char_before_cursor = line:sub(col, col)
-          if char_before_cursor:match("%s") or col == 0 then
-              return vim.api.nvim_replace_termcodes("<Tab>", true, false, true)
-          end
-          local ls = require("luasnip")
-          if ls.expand_or_jumpable() then
-            return "<Plug>luasnip-expand-or-jump"
-          end
-          return vim.api.nvim_replace_termcodes("<Esc>la", true, false, true)
-      end
-      vim.keymap.set('i', '<Tab>', [[v:lua.check_and_insert_space()]], {expr = true, remap = true})
+             -- Check if there's whitespace before the cursor
+             local char_before_cursor = line:sub(col, col)
+             if char_before_cursor:match("%s") or col == 0 then
+                 return vim.api.nvim_replace_termcodes("<Tab>", true, false, true)
+             end
+             local ls = require("luasnip")
+             if ls.expand_or_jumpable() then
+               return "<Plug>luasnip-expand-or-jump"
+             end
+             return vim.api.nvim_replace_termcodes("<Esc>la", true, false, true)
+         end
+         vim.keymap.set('i', '<Tab>', [[v:lua.check_and_insert_space()]], {expr = true, remap = true})
 
 
 
-      -- https://vi.stackexchange.com/questions/39596/neovim-augroup-and-autocommand-in-lua-relative-line-numbering
-      --  https://stackoverflow.com/questions/37552913/vim-how-to-keep-folds-on-save
-      local rememberFolds = vim.api.nvim_create_augroup("rememberFolds", {clear = true})
-      vim.api.nvim_create_autocmd(
-        { "BufWinLeave"},
-        {
-          group = rememberFolds,
-          pattern = "*",
-          command = "mkview"
-        }
-      )
-      vim.api.nvim_create_autocmd(
-        {"BufWinEnter"},
-        {
-          group = rememberFolds,
-          pattern = "*",
-          command = "silent! loadview"
-        }
-      )
+         -- https://vi.stackexchange.com/questions/39596/neovim-augroup-and-autocommand-in-lua-relative-line-numbering
+         --  https://stackoverflow.com/questions/37552913/vim-how-to-keep-folds-on-save
+         local rememberFolds = vim.api.nvim_create_augroup("rememberFolds", {clear = true})
+         vim.api.nvim_create_autocmd(
+           { "BufWinLeave"},
+           {
+             group = rememberFolds,
+             pattern = "*",
+             command = "mkview"
+           }
+         )
+         vim.api.nvim_create_autocmd(
+           {"BufWinEnter"},
+           {
+             group = rememberFolds,
+             pattern = "*",
+             command = "silent! loadview"
+           }
+         )
     '';
   };
 }
