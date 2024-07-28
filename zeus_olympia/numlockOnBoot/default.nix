381c57ad5c5c377cee4378568ce2689c88fbc35f
System administrator <you@example.com>
2024-07-28 19:35:04 -0300

    successful nixos-rebuild; testing numlock on boot

---
+++ zeus_olympia/numlockOnBoot/default.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  lib,
+  pkgs,
+  nixosVars,
+  ...
+}: let
+  cfg = config.${nixosVars.hostName}.system.numlockOnBoot;
+in {
+  options.${nixosVars.hostName}.system.numlockOnBoot.enable = lib.mkEnableOption "enable numlock on boot";
+
+  config = lib.mkIf cfg.enable {
+    boot.initrd.preLVMCommands = ''
+  ${pkgs.kbd}/bin/setleds +num
+'';
+      };
+    };
+  };
+}
