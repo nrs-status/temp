3e7c98c6e0c04774460128361285c8213145c412
System administrator <you@example.com>
2024-07-29 00:06:08 -0300

    successful nixos-rebuild; packaging swayrst

--- temple_artemis_ephesus/sway/customPackaging.nix
+++ temple_artemis_ephesus/sway/customPackaging.nix
@@ -5,7 +5,19 @@
 
     src = ./resources/packages/swayrst;
 
+    #phases must be explicitly declared otherwise will run an unpack phase that will attempt to extract the source
     phases = ["installPhase"];
+
+    #autoPatchelHook is used to patch link dependencies that the executable requires in order to run
+    nativeBuildInputs = [
+      pkgs.autoPatchelfHook
+    ];
+
+    #links to be patched go here; use ldd and objectdump -x to figure out which ones should come here
+    buildInputs = with pkgs; [
+      libz
+      glibc
+    ]
 
     installPhase = ''
       mkdir -p $out/bin
