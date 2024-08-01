41455dbce6ac51ab549c115c0d87863159a3947c
System administrator <you@example.com>
2024-08-01 11:38:36 -0300

    successful nixos-rebuild; patching ollama

---
+++ temple_artemis_ephesus/ai/customPackaging.nix
@@ -0,0 +1,12 @@
+{pkgs, ...}: {
+  ollama = pkgs.ollama.overrideAttrs (old: {
+    version = "0.3.2";
+    src = pkgs.fetchFromGitHub {
+    owner = "ollama";
+    repo = "ollama";
+    rev = "v${version}";
+    hash = "";
+    fetchSubmodules = true;
+    };
+  })
+}
