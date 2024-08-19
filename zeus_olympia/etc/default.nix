0d872f3e22c14c84e1f16b3568468d0a2772299e
System administrator <you@example.com>
2024-08-19 11:32:48 -0300

    successful nixos-rebuild; adding obelisk cache

--- zeus_olympia/etc/default.nix
+++ zeus_olympia/etc/default.nix
@@ -7,8 +7,8 @@ in
   options.${nixosVars.hostName}.system.etc.enable = lib.mkEnableOption "etc";
   config = lib.mkIf cfg.enable { 
     #these are used to make obelisk build faster
-    binaryCaches = [ "https://nixcache.reflex-frp.org" ];
-    binaryCachePublicKeys = [ "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" ];
+    nix.binaryCaches = [ "https://nixcache.reflex-frp.org" ];
+    nix.binaryCachePublicKeys = [ "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" ];
 
 
     };
