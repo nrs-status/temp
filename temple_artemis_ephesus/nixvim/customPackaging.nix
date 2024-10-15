f56d166fbf0809eb14273fe6b4618c28f1ce6ca2
System administrator <you@example.com>
2024-10-15 18:22:53 -0300

    successful nixos-rebuild; adding agda

--- temple_artemis_ephesus/nixvim/customPackaging.nix
+++ temple_artemis_ephesus/nixvim/customPackaging.nix
@@ -67,7 +67,7 @@
         owner = "ashinkarov";
         repo = "nvim-agda";
         rev = "9024909ac5cbac0a0b6f1f3f7f2b65c907c8fc12";
-        hash = "";
+        hash = "sha256-C2JWoCF2eeZFZ3J+1//FJ7FPqRE9w4CcwyEVI8vwZPw=";
     };
 })];
 
