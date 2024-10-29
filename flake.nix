65d4028fd1b5de9369c2db4ab6df7c043bffe86a
System administrator <you@example.com>
2024-10-29 18:19:24 -0300

    refactoring flake

--- flake.nix
+++ flake.nix
@@ -22,6 +22,8 @@
     };
 
     emacs-overlay.url = "github:nix-community/emacs-overlay/master";
+
+    nineveh.url = "github:nrs-status/nineveh";
   };
 
   outputs = inputs @ {
@@ -32,6 +34,7 @@
     vault-secrets,
     nix-doom-emacs-unstraightened,
     emacs-overlay,
+    nineveh,
     ...
   }: let
     env = import hanging_gardens_babylon/nineveh;
@@ -69,63 +72,6 @@
         program = pkgs.lib.getExe self.packages.hello;
       };
     };
-    nixosConfigurations = {
-      #the following variable name must be the current host's variable name, otherwise will raise an error
-      ${hostName} = nixpkgs.lib.nixosSystem {
-        system = env.nixosVars.system;
-        specialArgs = env;
-        modules = [
-          {
-            networking.hostName = env.nixosVars.hostName;
-            time.timeZone = env.nixosVars.timeZone;
-            nixpkgs.overlays = [
-              emacs-overlay.overlay
-              (final: prev: {
-              })
-              self.overlays.default
-            ];
-          }
-          ./zeus_olympia
-          #the vault-secrets.nixosModules.vault-secrets module call has this odd structure because the first vault-secrets is the flake from our inputs, it just turns out that the flake has the same name as the nixosModules attribute `vault-secrets`. Now, the flake `vault-secrets` has the attribute `nixosModules` because it is where it publishes a nixos module; in contrast see the flake `home-manager` that publishes, not only a nixosModules attribute but also a darwin module. The flake `nixvim` publishes a nixos module, a darwin module, and a home manager module.
-          #furthermore, it ends with `.vault-secrets`, an attribute of nixosModules, because it is possible to have a flake that publishes multiple nixosModules
-          vault-secrets.nixosModules.vault-secrets
-          {
-            options.nineveh.system.home-manager.enable = pkgs.lib.mkEnableOption "home manager";
-            config.${hostName}.system = helpers.stringListToEnabledOptions env.nixosVars.modulesToEnable; #in charge of setting the nixosModule options
-          }
-          # make home-manager as a module of nixos so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
-          home-manager.nixosModules.home-manager
-          ({
-            config,
-            lib,
-            ...
-          }: let
-            inherit env;
-            inherit hostName;
-            mainUser = env.nixosVars.mainUser;
-          in {
-            home-manager = lib.mkIf config.${hostName}.system.home-manager.enable {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              extraSpecialArgs = env;
-              users.${mainUser} = {
-                imports = [
-                  ./temple_artemis_ephesus
-                  nixvim.homeManagerModules.nixvim
-                  nix-doom-emacs-unstraightened.hmModule
-                ];
-                home = {
-                  username = mainUser;
-                  homeDirectory = "/home/${mainUser}";
-                  stateVersion = "23.11";
-                };
-                programs.home-manager.enable = true;
-                ${hostName}.home = helpers.stringListToEnabledOptions env.homeVars.pkgSets;
-              };
-            };
-          })
-        ];
-      };
-    };
+    nixosConfigurations = nineveh.nineveh
   };
 }
